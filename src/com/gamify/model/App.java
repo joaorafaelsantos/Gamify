74f20bff525d997bdb712072516a1e55067bee15
João Rafael Santos <joaorafael23@gmail.com>
2017-12-19 09:07:27 +0000

    App, Error and User Classes
    Creation of App, Error and User Classes
    com.gamify.model

--- src/com/gamify/model/App.java
+++ src/com/gamify/model/App.java
@@ -1,9 +1,66 @@
 package com.gamify.model;
 
 public class App {
-
-	public App() {
-		// TODO Auto-generated constructor stub
+	
+	private String idApp;
+	private User username;
+	private String appName;
+	private String type;
+	private String description;
+	
+	public App(String idApp, User username, String appName, String type, String description) {
+		super();
+		this.idApp = idApp;
+		this.username = username;
+		this.appName = appName;
+		this.type = type;
+		this.description = description;
 	}
+
+	public String getIdApp() {
+		return idApp;
+	}
+
+	public void setIdApp(String idApp) {
+		this.idApp = idApp;
+	}
+
+	public User getUsername() {
+		return username;
+	}
+
+	public void setUsername(User username) {
+		this.username = username;
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+	
+
+	
 
 }
--- src/com/gamify/model/Error.java
+++ src/com/gamify/model/Error.java
@@ -2,8 +2,53 @@ package com.gamify.model;
 
 public class Error {
 
-	public Error() {
-		// TODO Auto-generated constructor stub
+	private int code;
+	private String message;
+	private String link;
+	private int http_status;
+	
+	public Error(int code, String message, String link, int http_status) {
+		super();
+		this.code = code;
+		this.message = message;
+		this.link = link;
+		this.http_status = http_status;
 	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public int getHttp_status() {
+		return http_status;
+	}
+
+	public void setHttp_status(int http_status) {
+		this.http_status = http_status;
+	}
+	
+	
+	
+	
 
 }
