25690f655a1cdce565e7a559114b8bb1471d9596
João Rafael Santos <joaorafael23@gmail.com>
2017-12-19 10:35:50 +0000

    App, Error and User Resources
    Creation of App, Error and User Resources
    com.gamify.api

--- src/com/gamify/api/ErrorsResource.java
+++ src/com/gamify/api/ErrorsResource.java
@@ -1,9 +1,48 @@
 package com.gamify.api;
 
-public class ErrorsResource {
+import java.util.List;
 
-	public ErrorsResource() {
-		// TODO Auto-generated constructor stub
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import com.gamify.impl.ErrorManager;
+import com.gamify.model.Error;
+
+@Path("/errors")
+public class ErrorsResource {
+	
+	// Get all errors
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Error> getErrors() {
+		
+		ErrorManager em = ErrorManager.getInstance();		
+		return em.getErrors();
+	}
+	
+	// GET a specific error
+	@Path("/{idError}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Error getError(@PathParam("idError") String idError) {
+		
+		ErrorManager em = ErrorManager.getInstance();		
+		return em.getError(idError);
 	}
 
 }
+
+
+
--- src/com/gamify/api/UsersResource.java
+++ src/com/gamify/api/UsersResource.java
@@ -1,9 +1,75 @@
 package com.gamify.api;
 
-public class UsersResource {
+import java.util.List;
 
-	public UsersResource() {
-		// TODO Auto-generated constructor stub
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import com.gamify.impl.UserManager;
+import com.gamify.model.User;
+
+@Path("/users")
+public class UsersResource {
+	
+	// Create new user
+	@POST
+	@Consumes("application/x-www-form-urlencoded")
+	public Response createUser(
+			@FormParam("username") String username,
+			@FormParam("password") String password,
+			@FormParam("email") String email,
+			@Context UriInfo uriInfo) {
+		
+		UserManager gm = UserManager.getInstance();
+		gm.createUser(username, password, email);
+					
+		UriBuilder builder = uriInfo.getAbsolutePathBuilder();	
+		builder.path(username);
+		return Response.created(builder.build()).build();
+	}
+
+	// Get all users
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<User> getUsers() {
+		
+		UserManager um = UserManager.getInstance();		
+		return um.getUsers();
+	}
+	
+	// GET a specific user
+	@Path("/{username}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public User getUser(@PathParam("username") String username) {
+		
+		UserManager um = UserManager.getInstance();		
+		return um.getUser(username);
+	}
+	
+	// DELETE a specific user
+	@Path("/{username}")
+	@DELETE	
+	public Response removeUser(@PathParam("username") String username) {
+		
+		UserManager gm = UserManager.getInstance();		
+		gm.removeUser(username);
+		
+		return Response.ok().entity("User removed!").build();
 	}
 
 }
+
+
