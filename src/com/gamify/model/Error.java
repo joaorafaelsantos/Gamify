f3c405513300ee77778a4ea5511b3af314c65d93
João Rafael Santos <joaorafael23@gmail.com>
2017-12-19 09:45:37 +0000

    Class Error fix
    Change variable name 'code' to 'idError'

--- src/com/gamify/model/Error.java
+++ src/com/gamify/model/Error.java
@@ -2,27 +2,27 @@ package com.gamify.model;
 
 public class Error {
 
-	private int code;
+	private int idError;
 	private String message;
 	private String link;
 	private int http_status;
 	
-	public Error(int code, String message, String link, int http_status) {
+	public Error(int idError, String message, String link, int http_status) {
 		super();
-		this.code = code;
+		this.idError = idError;
 		this.message = message;
 		this.link = link;
 		this.http_status = http_status;
 	}
 
-	public int getCode() {
-		return code;
+	public int getIdError() {
+		return idError;
 	}
-
-	public void setCode(int code) {
-		this.code = code;
+	
+	public void setIdError(int idError) {
+		this.idError = idError;
 	}
-
+	
 	public String getMessage() {
 		return message;
 	}
@@ -46,9 +46,4 @@ public class Error {
 	public void setHttp_status(int http_status) {
 		this.http_status = http_status;
 	}
-	
-	
-	
-	
-
 }
