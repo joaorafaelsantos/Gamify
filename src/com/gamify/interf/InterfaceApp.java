5e153e0ec116d9ad58e0f720c4be7141728a4d40
João Rafael Santos <joaorafael23@gmail.com>
2017-12-20 13:40:21 +0000

    InterfaceApp fix
    Fix newApp variable type (App)

--- src/com/gamify/interf/InterfaceApp.java
+++ src/com/gamify/interf/InterfaceApp.java
@@ -10,7 +10,7 @@ public interface InterfaceApp {
 	public void createApp(String idApp, User username, String appName, String type, String description);
 	public List<App> getApps();
 	public App getApp(String idApp);
-	public void changeApp(String idApp, App oldApp, User newApp);
+	public void changeApp(String idApp, App oldApp, App newApp);
 	public void removeApp(String idApp);
 
 }
