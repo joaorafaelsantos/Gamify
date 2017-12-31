9c7e2a049a986822f316e936860f1f170eed40e0
Renato Costa <32844665+rcosta26@users.noreply.github.com>
2017-12-31 04:36:33 +0000

    AchievementResource
    Add an AchievementResource.class ready for some tests

--- src/com/gamify/api/AchievementsResource.java
+++ src/com/gamify/api/AchievementsResource.java
@@ -1,92 +1,100 @@
 package com.gamify.api;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import com.gamify.impl.AchievementManager;
+import com.gamify.model.Achievement;
+import com.gamify.model.App;
+
+
+
+
+@Path("/apps/{appID}/achievements")
 public class AchievementsResource {
 
-	public AchievementsResource() {
-		// TODO Auto-generated constructor stub
+	String userAuth = "joaorsantos"; // To change when add auth (token)
+
+	// Create new achievement
+	@POST
+	@Consumes("application/x-www-form-urlencoded")
+	public Response createAchievement(
+			@FormParam("achievementID") String achievementID,
+			@PathParam("appID") App appID,
+			@FormParam("achievementName") String achievementName,
+			@FormParam("structure") String structure,
+			@FormParam("goal") String goal,
+			@FormParam("type") String type,
+			@FormParam("description") String description,
+			@Context UriInfo uriInfo) {
+
+		AchievementManager am = AchievementManager.getInstance();
+
+		am.createAchievement(achievementID, appID, achievementName, structure, goal,type,description);
+
+		UriBuilder builder = uriInfo.getAbsolutePathBuilder();	
+		builder.path(achievementID);
+		return Response.created(builder.build()).build();
+	}
+
+
+	// Get all achievement
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Achievement> getAchievements(String userAuth) {
+
+		AchievementManager am = AchievementManager.getInstance();		
+		return am.getAchievements(userAuth);
+	}
+
+	// GET a specific achievement
+	@Path("/{achievementID}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Achievement getAchievement(String userAuth, @PathParam("achievementID") String achievementID) {
+
+		AchievementManager am = AchievementManager.getInstance();		
+		return am.getAchievement(userAuth,achievementID);
+	}
+
+	// Change a specific achievement
+	@Path("/{achievementID}")
+	@POST
+	@Consumes("application/x-www-form-urlencoded")
+	
+	public Response changeAchievement(@PathParam("achievementID") String achievementID, @FormParam("achievementName") String achievementName, @FormParam("goal") String goal, @FormParam("type") String type, @FormParam("description") String description) {
+
+		AchievementManager am = AchievementManager.getInstance();		
+		am.changeAchievement(achievementID, achievementName,goal, type, description);
+
+		return Response.ok().entity("").build(); // Send response * TO DO *
+	}
+	
+	//DELETE a specific achievement
+	@Path("/{achievementID}")
+	@DELETE	
+	public Response removeAchievement(@PathParam("achievementID") String achievementID) {
+
+		AchievementManager am = AchievementManager.getInstance();		
+		am.removeAchievement(achievementID);
+
+		return Response.ok().entity("").build(); // Send response * TO DO *
 	}
 
 }
 
 
-//package com.gamify.api;
-//
-//
-//import java.util.List;
-//
-//import javax.ws.rs.Consumes;
-//import javax.ws.rs.DELETE;
-//import javax.ws.rs.FormParam;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.POST;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.PathParam;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.QueryParam;
-//import javax.ws.rs.core.Context;
-//import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
-//import javax.ws.rs.core.UriBuilder;
-//import javax.ws.rs.core.UriInfo;
-//
-//import com.gamify.impl.GameManager;
-//import com.gamify.model.Game;
-//
-//@Path("/games")
-//public class GamesResource {
-//
-//	// GET all the games
-//	@GET
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public List<Game> getGames(@QueryParam("category") String category) {
-//		
-//		GameManager gm = GameManager.getInstance();		
-//		if (category!=null) {
-//			return gm.getGames(category);
-//		} 
-//		return gm.getGames();
-//	}
-//	
-//	// GET a specific game
-//	@Path("/{gameId}")
-//	@GET
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Game getGame(@PathParam("gameId") String title) {
-//		
-//		GameManager gm = GameManager.getInstance();		
-//		return gm.getGame(title);
-//	}
-//	
-//	// DELETE a specific game
-//	@Path("/{gameId}")
-//	@DELETE	
-//	public Response removeGame(@PathParam("gameId") String title) {
-//		
-//		GameManager gm = GameManager.getInstance();		
-//		gm.removeGame(title);
-//		
-//		return Response.ok().entity("Game removed!").build();
-//	}
-//	
-//	//Post a new game
-//	@POST
-//	@Consumes("application/x-www-form-urlencoded")
-//	public Response insertGame(
-//			@FormParam("title") String title,
-//			@FormParam("company") String company,
-//			@FormParam("year") int year,
-//			@FormParam("category") String category,
-//			@Context UriInfo uriInfo) {
-//		
-//		GameManager gm = GameManager.getInstance();
-//		gm.createGame(title, company, year, category);
-//					
-//		UriBuilder builder = uriInfo.getAbsolutePathBuilder();	
-//		builder.path(title);
-//		return Response.created(builder.build()).build();
-//	}
-//	
-//}
-//
-//
-//
+
